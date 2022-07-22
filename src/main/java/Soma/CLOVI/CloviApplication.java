2a1f8053adf73d59bda335790478785264afc576
voka <chdlswhd7@naver.com>
2022-07-22 13:28:10 +0900

    Init: init Repository

---
+++ src/main/java/Soma/CLOVI/CloviApplication.java
@@ -0,0 +1,13 @@
+package Soma.CLOVI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloviApplication.class, args);
+	}
+
+}
