aba57bd654423531c235a538c9fffb5719eeff0c
JaeSeok Oh <chdlswhd7@naver.com>
2022-08-21 12:50:13 +0900

    Feat: 모델 목록 보내주는 기능 추가

---
+++ src/main/java/Soma/CLOVI/api/controller/ModelController.java
@@ -0,0 +1,26 @@
+package Soma.CLOVI.api.controller;
+
+import Soma.CLOVI.api.response.BaseResponse;
+import Soma.CLOVI.api.response.MessageCode;
+import Soma.CLOVI.api.response.ProcessStatus;
+import Soma.CLOVI.domain.Model;
+import Soma.CLOVI.dto.use.VideoResponseDto;
+import Soma.CLOVI.repository.ModelRepository;
+import Soma.CLOVI.service.ModelService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class ModelController {
+    private final ModelService modelService;
+
+    @GetMapping("/api/v1/models")
+    public BaseResponse models() {
+        return new BaseResponse(modelService.searchList(), HttpStatus.OK, ProcessStatus.SUCCESS, MessageCode.SUCCESS_GET_LIST);
+    }
+}
