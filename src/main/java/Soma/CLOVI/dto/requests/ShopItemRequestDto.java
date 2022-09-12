31a524755944cf9bedd999ec1eb14167070504d6
voka <chdlswhd7@naver.com>
2022-09-12 17:34:48 +0900

    Feat: shopItem 저장 기능 개발  (TSP-337)

---
+++ src/main/java/Soma/CLOVI/dto/requests/ShopItemRequestDto.java
@@ -0,0 +1,16 @@
+package Soma.CLOVI.dto.requests;
+
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+public class ShopItemRequestDto {
+  private Long shopId;
+  private Long itemId;
+
+  private String shopName;
+  private String shopUrl;
+  private String imgUrl;
+
+  private Long price;
+}
--- src/main/java/Soma/CLOVI/service/ShopItemService.java
+++ src/main/java/Soma/CLOVI/service/ShopItemService.java
@@ -1,8 +1,48 @@
 package Soma.CLOVI.service;
 
+import Soma.CLOVI.domain.ManyToMany.ShopItem;
+import Soma.CLOVI.domain.item.Item;
+import Soma.CLOVI.domain.shop.Shop;
+import Soma.CLOVI.dto.requests.ShopItemRequestDto;
+import Soma.CLOVI.dto.use.TimeItemRequestDto;
+import Soma.CLOVI.repository.Item.ItemRepository;
+import Soma.CLOVI.repository.ShopItemRepository;
+import Soma.CLOVI.repository.ShopRepository;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ShopItemService {
 
+  private final ShopService shopService;
+  private final ItemService itemService;
+  private final ItemRepository itemRepository;
+  private final ShopItemRepository shopItemRepository;
+  @Transactional
+  public Long save(ShopItemRequestDto shopItemRequestDto) {
+//    if(isExistUrl(shopItemRequestDto.getShopUrl())){
+//      throw new IllegalArgumentException("이미 존재하는 판매처 입니다.");
+//    }
+    System.out.println(shopItemRequestDto);
+
+    Item item = itemService.getById(shopItemRequestDto.getItemId());
+    Shop shop = shopService.getById(shopItemRequestDto.getShopId());
+
+    ShopItem shopItem = new ShopItem(shopItemRequestDto,item,shop);
+    shopItemRepository.save(shopItem);
+
+    return shopItem.getId();
+  }
+
+  public boolean isExistUrl(String shopUrl){
+    Optional<ShopItem> shopItem = shopItemRepository.findByShopUrl(shopUrl);
+    if(shopItem.isPresent()){
+      return true;
+    }
+    return false;
+  }
 }
