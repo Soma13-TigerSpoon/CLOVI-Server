d6244111f29a9b3f179fc27629adcc2c42a24226
Hyung Goo Lee <hgl9023@naver.com>
2022-10-19 21:52:07 +0900

    Feat: Add videoUrl to VideoResponseDto

--- src/main/java/Soma/CLOVI/dto/response/VideoResponseDto.java
+++ src/main/java/Soma/CLOVI/dto/response/VideoResponseDto.java
@@ -8,23 +8,22 @@ import lombok.Getter;
 
 @Getter
 public class VideoResponseDto {
-
   private Long id;
   private String creator;
-
   private String profileImgUrl;
-
-  private List<TimeShopItemResponseDto> lists = new ArrayList<>();
+  private String videoUrl;
+  private List<TimeShopItemResponseDto> timeShopItemLists = new ArrayList<>();
 
   public VideoResponseDto(Video video) {
     this.id = video.getId();
-    this.creator = video.getChannel().getName(); // Select Channel
-    this.profileImgUrl = video.getChannel().getProfileImgUrl(); // Select Channel Image
+    this.creator = video.getChannel().getName(); // select channel -> name
+    this.profileImgUrl = video.getChannel().getProfileImgUrl(); // select channel -> profile image url
+    this.videoUrl = video.getVideoUrl(); // select video url
 
-    List<TimeFrame> timeFrames = video.getTimeFrames(); // Select TimeFrame
-    if (!timeFrames.isEmpty()) {
-      for (TimeFrame timeFrame : video.getTimeFrames()) {
-        this.lists.add(new TimeShopItemResponseDto(timeFrame));
+    List<TimeFrame> timeFrames = video.getTimeFrames(); // select timeFrames
+    if(!timeFrames.isEmpty()) {
+      for(TimeFrame timeFrame : video.getTimeFrames()) {
+        this.timeShopItemLists.add(new TimeShopItemResponseDto(timeFrame));
       }
     }
   }
