4c67c259ab66d4bd062ed7e69a97f04010a36b46
voka <chdlswhd7@naver.com>
2023-02-21 21:02:43 +0900

    Feat: Custom Exception 들 선언 및 Exception을 Controller 할 수 있게 ControllerAdvice 추가 , Exception 발생하면 반환하는 형식 ErrorReponse 추가

---
+++ src/main/java/com/clovi/exception/member/DuplicateMemberIdException.java
@@ -0,0 +1,13 @@
+package com.clovi.exception.member;
+
+import com.clovi.exception.BadRequestException;
+
+public class DuplicateMemberIdException extends BadRequestException {
+  private static final String SERVER_MESSAGE = "이미 존재하는 아이디 입니다.";
+  private static final String ERROR_CODE = "MEMBER_ID_EXISTS";
+  private static final String CLIENT_MESSAGE = "이미 존재하는 아이디 입니다. 다른 아이디를 입력해주세요!";
+
+  public DuplicateMemberIdException() {
+    super(SERVER_MESSAGE, CLIENT_MESSAGE, ERROR_CODE);
+  }
+}
