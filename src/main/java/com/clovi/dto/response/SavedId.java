e514e97e9098e66d1e3fc4e9e4d36ab72311cdbf
voka <chdlswhd7@naver.com>
2023-02-21 20:53:40 +0900

    Update: 회원가입 RequestDto 및 반환 Dto 작성

---
+++ src/main/java/com/clovi/dto/response/SavedId.java
@@ -0,0 +1,15 @@
+package com.clovi.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+
+@Getter
+@Schema(name = "객체 저장 후 식별 id 응답")
+public class SavedId {
+  @Schema(description = "id")
+  private Long id;
+
+  public SavedId(Long id) {
+    this.id = id;
+  }
+}
