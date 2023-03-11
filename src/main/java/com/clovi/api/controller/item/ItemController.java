bd90003a6854696d93e8cb6a2812a88f0ddc7e4f
voka <chdlswhd7@naver.com>
2023-03-11 13:38:04 +0900

    Update: Controller 이름 변경

--- src/main/java/com/clovi/api/controller/item/ItemController.java
+++ src/main/java/com/clovi/api/controller/item/ItemController.java
@@ -3,78 +3,51 @@ package com.clovi.api.controller.item;
 import com.clovi.api.response.BaseResponse;
 import com.clovi.api.response.MessageCode;
 import com.clovi.api.response.ProcessStatus;
-import com.clovi.dto.requests.item.ItemCreateRequest;
-import com.clovi.dto.requests.item.ItemDeleteRequest;
-import com.clovi.dto.requests.item.ItemUpdateRequest;
+import com.clovi.dto.requests.item.detail.ItemCreateRequest;
+import com.clovi.dto.requests.item.detail.ItemDeleteRequest;
+import com.clovi.dto.requests.item.detail.ItemUpdateRequest;
 import com.clovi.dto.response.IdResponseDto;
-import com.clovi.dto.requests.TimeItemRequestDto;
-import com.clovi.dto.response.ItemResponseDto;
 import com.clovi.service.item.ItemService;
-import com.clovi.service.query.ItemQueryService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.net.URI;
-import java.util.List;
-import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
-@Tag(name = "아이템 API")
+@Tag(name = "유튜버가 입력하는 아이템 색,사이즈 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
 public class ItemController {
+
   private final ItemService itemService;
-  private final ItemQueryService itemQueryService;
-
-  @GetMapping("/item/{item_id}")
-  public ResponseEntity getOneItem(@Validated @PathVariable(name = "item_id") Long itemId) {
-    ItemResponseDto result = itemService.getItemById(itemId);
-
-    if(result == null) {
-      return ResponseEntity.badRequest().body(
-              new BaseResponse(HttpStatus.BAD_REQUEST.value(), ProcessStatus.FAIL, MessageCode.ERROR_REQ_PARAM_ITEM_ID)
-      );
-    }
-
-    return ResponseEntity.ok(
-            new BaseResponse(result, HttpStatus.OK.value(), ProcessStatus.SUCCESS, MessageCode.SUCCESS_GET)
-    );
-  }
-
-  @PostMapping("/items")
-  public ResponseEntity saveItem(@Valid @RequestBody TimeItemRequestDto timeItemRequestDto) {
-    // System.out.println(timeItemRequestDto);
-    Long savedId = itemQueryService.save(timeItemRequestDto);
-
-    return new ResponseEntity<>(
-            new BaseResponse(new IdResponseDto(savedId), HttpStatus.CREATED.value(), ProcessStatus.SUCCESS, MessageCode.SUCCESS_CREATE),
-            HttpStatus.CREATED
-    );
-  }
-  @PostMapping("/item")//아이템 생성 API
-  @Operation(summary = "아이템 생성", description = "아이템을 직접 입력해 저장한다.")
-  public ResponseEntity createItem(@Validated @RequestBody ItemCreateRequest itemCreateRequest, Long userId){
+  @PostMapping("/items/detail")//아이템 상세 생성 API
+  @Operation(summary = "아이템 상세 생성", description = "아이템 상세를 저장한다.")
+  public ResponseEntity createItemDetail(@Validated @RequestBody ItemCreateRequest itemCreateRequest, Long userId){
     IdResponseDto savedId = new IdResponseDto(itemService.create(itemCreateRequest,userId));
     return ResponseEntity.created(
-        URI.create("/api/v1/evaluation/" + savedId.getSavedId())).body(new BaseResponse(savedId, HttpStatus.CREATED.value(),ProcessStatus.SUCCESS,
+        URI.create("/api/v1/evaluation/" + savedId.getSavedId())).body(new BaseResponse(savedId, HttpStatus.CREATED.value(),
+        ProcessStatus.SUCCESS,
         MessageCode.SUCCESS_CREATE));
   }
-  @PutMapping("/item")//아이템 수정 API
-  @Operation(summary = "아이템 수정", description = "아이템을 직접 입력해 수정한다.")
-  public ResponseEntity updateItem(@Validated @RequestBody ItemUpdateRequest itemUpdateRequest, Long userId){
+  @PutMapping("/items/detail")//아이템 상세 수정 API
+  @Operation(summary = "아이템 상세 수정", description = "아이템 상세를 수정한다.")
+  public ResponseEntity updateItemDetail(@Validated @RequestBody ItemUpdateRequest itemUpdateRequest, Long userId){
     IdResponseDto savedId = new IdResponseDto(itemService.update(itemUpdateRequest,userId));
     return ResponseEntity.ok(new BaseResponse(savedId,HttpStatus.OK.value(),ProcessStatus.SUCCESS, MessageCode.SUCCESS_UPDATE));
   }
-  @DeleteMapping("/item")//아이템 삭제 API
-  @Operation(summary = "아이템 삭제", description = "아이템을 삭제한다.")
-  public ResponseEntity deleteItem(@Validated @RequestBody ItemDeleteRequest itemDeleteRequest, Long userId){
+  @DeleteMapping("/items/detail")//아이템 상세 삭제 API
+  @Operation(summary = "아이템 상세 삭제", description = "아이템 상세를 삭제한다.")
+  public ResponseEntity deleteItemDetail(@Validated @RequestBody ItemDeleteRequest itemDeleteRequest, Long userId){
     itemService.delete(itemDeleteRequest,userId);
     return ResponseEntity.ok(new BaseResponse(HttpStatus.OK.value(),ProcessStatus.SUCCESS, MessageCode.SUCCESS_DELETE));
   }
-
 }
