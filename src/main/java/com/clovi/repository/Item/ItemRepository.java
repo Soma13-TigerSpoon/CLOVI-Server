ba0b70032c26f3eb83da4b7f7fd4e9e4efb0e85d
voka <chdlswhd7@naver.com>
2023-03-11 13:53:52 +0900

    Update: Item, ItemInfo 엔티티 변경으로 관련된 Respository 수정
    ItemDetail 삭제
    ItemRepository -> ItemInfoRespository
    color, size Repository에서 사용하던 itemId -> itemInfoId
    queryDsl -> item.category -> item.itemInfo.category

--- src/main/java/com/clovi/repository/Item/ItemRepository.java
+++ src/main/java/com/clovi/repository/Item/ItemRepository.java
@@ -1,14 +1,17 @@
 package com.clovi.repository.Item;
 
+import java.util.List;
+import java.util.Optional;
+
 import com.clovi.domain.item.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+public interface ItemRepository extends JpaRepository<Item, Long> {
+  Optional<Item> findByIdAndDeletedIsFalse(Long itemId);
 
-public interface ItemRepository extends JpaRepository<Item,Long> {
-   // Optional<Item> findById(String itemId);
-   // Optional<Item> findByName(String name);
-   Optional<Item> findByNameAndBrand(String name,String brand);
+  Optional<Item> findByItemInfoIdAndDeletedIsFalse(Long itemInfoId);
 
-   Optional<Item> findByIdAndIsDeletedIsFalse(Long itemId);
+  Optional<Item> findByItemInfoIdAndSizeAndDeletedIsFalse(Long itemInfoId, String size);
+
+  List<Item> findAllByItemInfoIdAndDeletedIsFalse(Long id);
 }
