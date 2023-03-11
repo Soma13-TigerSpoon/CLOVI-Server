7e5b304adcac68a70a47efe267d896322d38b1f2
voka <chdlswhd7@naver.com>
2023-03-11 13:27:07 +0900

    Feat: Delete 함수 BaseEntity에 정의해서 모든 엔티티에서 사용할 수 있도록 변경 및 ItemDetail 삭제로 TimeShopItem 생성자 수정

--- src/main/java/com/clovi/domain/ManyToMany/TimeShopItem.java
+++ src/main/java/com/clovi/domain/ManyToMany/TimeShopItem.java
@@ -3,7 +3,6 @@ package com.clovi.domain.ManyToMany;
 import com.clovi.domain.Base.BaseTimeEntity;
 import com.clovi.domain.TimeFrame;
 import com.clovi.domain.item.Item;
-import com.clovi.domain.item.ItemDetail;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -12,6 +11,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
+
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -26,20 +26,15 @@ public class TimeShopItem extends BaseTimeEntity {
   private Long id;
   @ManyToOne(fetch = FetchType.LAZY)
   private TimeFrame time;
-
   @ManyToOne(fetch = FetchType.LAZY)
   private Item item;
-
   @OneToOne(fetch = FetchType.LAZY)
   private ShopItem shopItem;
 
-  @OneToOne(fetch = FetchType.LAZY)
-  private ItemDetail itemDetail;
-  public TimeShopItem(TimeFrame time, Item item, ShopItem shopItem, ItemDetail itemDetail) {
+  public TimeShopItem(TimeFrame time, Item item, ShopItem shopItem) {
     this.time = time;
     this.item = item;
     this.shopItem = shopItem;
-    this.itemDetail = itemDetail;
   }
 
 }
