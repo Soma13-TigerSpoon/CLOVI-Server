96798dc3ca62ea0e44aa766734b61af0585d6dc8
voka <chdlswhd7@naver.com>
2023-04-02 23:11:11 +0900

    개발 단계에서는 모든 메서드와 헤더를 허용하고, Security Config에서 cors까지 같이 관리하면 좋을 거 같아 설정 변경

--- src/main/java/com/clovi/config/SecurityConfig.java
+++ src/main/java/com/clovi/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.clovi.config.filter.JwtFilter;
 import com.clovi.service.auth.MemberDetailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,18 +14,27 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+  @Value("${allow-cors.list}")
+  private List<String> allowCorsUrl;
   private final JwtTokenProvider jwtTokenProvider;
   private final MemberDetailService memberDetailService;
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
     httpSecurity
             .csrf().disable()
+            .cors()
+            .and()
        // Spring Security에서 session을 생성하거나 사용하지 않도록 설정
             .sessionManagement()
               .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
@@ -45,5 +55,19 @@ public class SecurityConfig {
   public JwtFilter tokenAuthenticationFilter() {
     return new JwtFilter(jwtTokenProvider,memberDetailService);
   }
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
 
+    configuration.addAllowedHeader("*");
+    configuration.addAllowedMethod("*");
+    configuration.setAllowCredentials(true);
+    for(String allowedUrl : allowCorsUrl){
+      configuration.addAllowedOriginPattern(allowedUrl);
+    }
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+  }
 }
